44eaa705385f10944855eea97138e3b3318e3010
fernanda vilches <fernanada.lohse@gmail.com>
2018-06-25 18:11:28 -0400

    prueba~

--- src/data.js
+++ src/data.js
@@ -40,21 +40,24 @@
         for (let i = 0; i < users.length; i++) {
             let userId = users[i].id;
             let progress = progres[userId];
-
-            let usersData = [];
-            let progressData = {};
-            let cohortData = {};
-            let cohortUsers = [];
-            let userStats = [];
-            let cohorts = {};
-            let courses = [];
-            let userByCohort = [];
+            console.log(progress);
+            
+            let percent = 0;
+            let readCompletado = 0;
+            let readTotal = 0;
+            let quizCompletada = 0;
+            let quizTotal = 0;
+            let practiceCompletada = 0;
+            let practiceTotal = 0;
+            let scoreSuma = 0;
  
             for(let j=0; j < course.length; j++){
 
-                console.log(course[j]);
+               // console.log(course[j]);
 
-                console.log(progress);
+                
+            
+
             }
         }
     }
